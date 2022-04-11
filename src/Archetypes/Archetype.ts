e190af14c2f720f38ac79fd6b4fa55ab3bb087ca
Rafael <rafaelmoura7033@gmail.com>
2022-04-11 18:51:16 -0300

    Consertando o nome do metodo static

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -19,7 +19,7 @@ export default abstract class Archetype {
     return this._cost;
   }
 
-  static createdRacesInstances(): number {
+  static createdArchetypeInstances(): number {
     throw new Error('Not implemented');
   }
 
